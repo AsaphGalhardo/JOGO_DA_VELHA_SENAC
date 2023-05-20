a71703a1d43f7c92b75835458f01f41bf2414df9
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-05-19 21:39:53 -0300

    Novo Update

--- JOGO_DA_VELHA_SENAC/Form1.Designer.cs
+++ JOGO_DA_VELHA_SENAC/Form1.Designer.cs
@@ -38,115 +38,216 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelXPONTOS = new System.Windows.Forms.Label();
+            this.label0PONTOS = new System.Windows.Forms.Label();
+            this.labelEMAPTE = new System.Windows.Forms.Label();
+            this.labelPONTOS = new System.Windows.Forms.Label();
+            this.labelOPONTOS = new System.Windows.Forms.Label();
+            this.labelEMPATES = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 477);
+            this.button1.Location = new System.Drawing.Point(181, 395);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(317, 31);
+            this.button1.Size = new System.Drawing.Size(225, 31);
             this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
+            this.button1.Text = "Limpar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(33, 24);
+            this.button2.Location = new System.Drawing.Point(14, 15);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 103);
-            this.button2.TabIndex = 10;
+            this.button2.Size = new System.Drawing.Size(92, 94);
+            this.button2.TabIndex = 0;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(225, 24);
+            this.button3.Location = new System.Drawing.Point(150, 15);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(111, 103);
-            this.button3.TabIndex = 11;
+            this.button3.Size = new System.Drawing.Size(100, 94);
+            this.button3.TabIndex = 1;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(402, 24);
+            this.button4.Location = new System.Drawing.Point(294, 15);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(111, 103);
-            this.button4.TabIndex = 12;
+            this.button4.Size = new System.Drawing.Size(99, 94);
+            this.button4.TabIndex = 2;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button2_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(33, 188);
+            this.button5.Location = new System.Drawing.Point(14, 145);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(111, 103);
-            this.button5.TabIndex = 13;
+            this.button5.Size = new System.Drawing.Size(92, 93);
+            this.button5.TabIndex = 3;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button2_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(225, 188);
+            this.button6.Location = new System.Drawing.Point(150, 145);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(111, 103);
-            this.button6.TabIndex = 14;
+            this.button6.Size = new System.Drawing.Size(100, 93);
+            this.button6.TabIndex = 4;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button2_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(402, 188);
+            this.button7.Location = new System.Drawing.Point(294, 145);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(111, 103);
-            this.button7.TabIndex = 15;
+            this.button7.Size = new System.Drawing.Size(99, 93);
+            this.button7.TabIndex = 5;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button2_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(33, 319);
+            this.button8.Location = new System.Drawing.Point(14, 264);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(111, 103);
-            this.button8.TabIndex = 16;
+            this.button8.Size = new System.Drawing.Size(92, 85);
+            this.button8.TabIndex = 6;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button2_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(225, 319);
+            this.button9.Location = new System.Drawing.Point(150, 264);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(111, 103);
-            this.button9.TabIndex = 17;
+            this.button9.Size = new System.Drawing.Size(100, 85);
+            this.button9.TabIndex = 7;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button2_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(402, 319);
+            this.button10.Location = new System.Drawing.Point(294, 264);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(111, 103);
-            this.button10.TabIndex = 18;
+            this.button10.Size = new System.Drawing.Size(99, 85);
+            this.button10.TabIndex = 8;
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(88, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 367);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.labelEMPATES);
+            this.panel3.Controls.Add(this.labelOPONTOS);
+            this.panel3.Controls.Add(this.labelPONTOS);
+            this.panel3.Controls.Add(this.labelEMAPTE);
+            this.panel3.Controls.Add(this.label0PONTOS);
+            this.panel3.Controls.Add(this.labelXPONTOS);
+            this.panel3.Location = new System.Drawing.Point(52, 446);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(552, 87);
+            this.panel3.TabIndex = 12;
+            this.panel3.Tag = "O";
+            // 
+            // labelXPONTOS
+            // 
+            this.labelXPONTOS.AutoSize = true;
+            this.labelXPONTOS.BackColor = System.Drawing.Color.Transparent;
+            this.labelXPONTOS.Font = new System.Drawing.Font("Quicksand", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelXPONTOS.Location = new System.Drawing.Point(15, 0);
+            this.labelXPONTOS.Name = "labelXPONTOS";
+            this.labelXPONTOS.Size = new System.Drawing.Size(107, 31);
+            this.labelXPONTOS.TabIndex = 0;
+            this.labelXPONTOS.Text = "X ganhou";
+            // 
+            // label0PONTOS
+            // 
+            this.label0PONTOS.AutoSize = true;
+            this.label0PONTOS.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label0PONTOS.Font = new System.Drawing.Font("Quicksand", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label0PONTOS.Location = new System.Drawing.Point(212, 0);
+            this.label0PONTOS.Name = "label0PONTOS";
+            this.label0PONTOS.Size = new System.Drawing.Size(106, 31);
+            this.label0PONTOS.TabIndex = 1;
+            this.label0PONTOS.Text = "0 ganhou";
+            // 
+            // labelEMAPTE
+            // 
+            this.labelEMAPTE.AutoSize = true;
+            this.labelEMAPTE.Font = new System.Drawing.Font("Quicksand", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEMAPTE.Location = new System.Drawing.Point(428, 0);
+            this.labelEMAPTE.Name = "labelEMAPTE";
+            this.labelEMAPTE.Size = new System.Drawing.Size(90, 31);
+            this.labelEMAPTE.TabIndex = 2;
+            this.labelEMAPTE.Text = "Empate";
+            // 
+            // labelPONTOS
+            // 
+            this.labelPONTOS.AutoSize = true;
+            this.labelPONTOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPONTOS.Location = new System.Drawing.Point(32, 47);
+            this.labelPONTOS.Name = "labelPONTOS";
+            this.labelPONTOS.Size = new System.Drawing.Size(18, 20);
+            this.labelPONTOS.TabIndex = 3;
+            this.labelPONTOS.Text = "0";
+            // 
+            // labelOPONTOS
+            // 
+            this.labelOPONTOS.AutoSize = true;
+            this.labelOPONTOS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOPONTOS.Location = new System.Drawing.Point(235, 47);
+            this.labelOPONTOS.Name = "labelOPONTOS";
+            this.labelOPONTOS.Size = new System.Drawing.Size(21, 20);
+            this.labelOPONTOS.TabIndex = 4;
+            this.labelOPONTOS.Text = "O";
+            // 
+            // labelEMPATES
+            // 
+            this.labelEMPATES.AutoSize = true;
+            this.labelEMPATES.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEMPATES.Location = new System.Drawing.Point(469, 47);
+            this.labelEMPATES.Name = "labelEMPATES";
+            this.labelEMPATES.Size = new System.Drawing.Size(18, 20);
+            this.labelEMPATES.TabIndex = 5;
+            this.labelEMPATES.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(544, 545);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(661, 545);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -162,6 +263,14 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelEMAPTE;
+        private System.Windows.Forms.Label label0PONTOS;
+        private System.Windows.Forms.Label labelXPONTOS;
+        private System.Windows.Forms.Label labelPONTOS;
+        private System.Windows.Forms.Label labelOPONTOS;
+        private System.Windows.Forms.Label labelEMPATES;
     }
 }
 
--- JOGO_DA_VELHA_SENAC/Form1.cs
+++ JOGO_DA_VELHA_SENAC/Form1.cs
@@ -15,6 +15,9 @@ namespace JOGO_DA_VELHA_SENAC
         public Form1()
         {
             InitializeComponent();
+            labelPONTOS.Text = "";
+            labelOPONTOS.Text = "";
+            labelEMPATES.Text = "";
         }
 
         int Xpontos = 0;
@@ -22,29 +25,141 @@ namespace JOGO_DA_VELHA_SENAC
         int rodadas = 0;
         int Opontos = 0;
         bool turno = true;
-        bool jogo_final = false;    
-
+        bool jogo_final = false;
+        string[] texto = new string[9];
+        private int playerqueganhou;
+        int xplayer = 0;
+        int oplayer = 0;
         private void button2_Click(object sender, EventArgs e)
         {
-
-
             button2 = (Button)sender;
+            int ButtonIndex = button2.TabIndex;
+
             if (button2.Text == "" && jogo_final == false)
             {
                 if (turno)
                 {
-                    button2.Text = "x";
+                    button2.Text = "X";
+                    texto[ButtonIndex] = button2.Text;
                     rodadas++;
                     turno = !turno;
+                    checagem(1);
                 }
+
                 else
                 {
-                    button2.Text = "o";
+                    button2.Text = "O";
+                    texto[ButtonIndex] = button2.Text;
                     rodadas++;
                     turno = !turno;
+                    checagem(2);
+                }
+
+            }
+        }
+        void vencedor(int playerqueganhou)
+        {
+
+            jogo_final = true;
+            if (playerqueganhou == 1)
+            {
+                xplayer++;
+                labelPONTOS.Text = Convert.ToString(xplayer);
+                MessageBox.Show("Jogador X ganhou!");
+                turno = true;
+            }
+            else
+            {
+                Opontos++;
+                labelOPONTOS.Text = Convert.ToString(oplayer);
+                MessageBox.Show("Jogador O ganhou!");
+                turno = false;
+            }
+
+        }
+        void checagem(int checagemplayer)
+        {
+            string suporte;
+            if (checagemplayer == 1)
+            {
+                suporte = "X";
+            }
+            else
+            {
+                suporte = "O";
+            }
+            for (int horizontal = 0; horizontal < 8; horizontal += 3)
+            {
+                if (suporte == texto[horizontal])
+                {
+                    if (texto[horizontal] == texto[horizontal + 1] && texto[horizontal] == texto[horizontal + 2])
+                    {
+                        vencedor(checagemplayer);
+                        return;
+                    }
+
+
                 }
             }
-        
+            for (int vertical = 0; vertical < 3; vertical ++)
+            {
+                if (suporte == texto[vertical])
+                {
+                    if (texto[vertical] == texto[vertical + 3] && texto[vertical] == texto[vertical + 6])
+                    {
+                        vencedor(checagemplayer);
+                        return;
+                    }
+
+
+                }
+            }
+
+            if (texto[0] == suporte)
+            {
+                if (texto[0] == texto[4] && texto[0] == texto[8])
+                {
+                    vencedor(checagemplayer);
+                    return;
+                }
+            }
+            if (texto[2] == suporte)
+            {
+                if (texto[2] == texto[4] && texto[2] == texto[6])
+                {
+                    vencedor(checagemplayer);
+                    return;
+                }
+            }
+            if (rodadas ==9 && jogo_final == false)
+            {
+                empates++;
+                labelEMPATES.Text = Convert.ToString(empates);
+                MessageBox.Show("Empate");
+                jogo_final = true;
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Text= "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button10.Text = "";
+            rodadas = 0;
+            jogo_final=false;
+
+           for (int i =0; i<9; i++)
+            {
+                texto[i] = "";
+            }
         }
     }
+
 }
